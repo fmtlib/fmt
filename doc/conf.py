b1da947d759bca2daf4629d33bc70f302d270260
Victor Zverovich <victor.zverovich@gmail.com>
2012-12-19 09:17:20 -0800

    Preserve original copyright.

--- doc/conf.py
+++ doc/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'format'
-copyright = u'2012, Victor Zverovich'
+copyright = u'1990-2012, Python Software Foundation'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
