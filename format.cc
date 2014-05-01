9830c528161f87d8a7177d3bbf4783c0d91714c4
Victor Zverovich <victor.zverovich@gmail.com>
2014-05-01 07:20:38 -0700

    Fix UTF8ToUTF16.

--- format.cc
+++ format.cc
@@ -176,12 +176,12 @@ fmt::internal::UTF8ToUTF16::UTF8ToUTF16(fmt::StringRef s) {
       CP_UTF8, MB_ERR_INVALID_CHARS, s.c_str(), -1, 0, 0);
   static const char ERROR[] = "cannot convert string from UTF-8 to UTF-16";
   if (length == 0)
-    ThrowSystemError(GetLastError(), ERROR);
+    ThrowWinError(GetLastError(), ERROR);
   buffer_.resize(length);
   length = MultiByteToWideChar(
     CP_UTF8, MB_ERR_INVALID_CHARS, s.c_str(), -1, &buffer_[0], length);
   if (length == 0)
-    ThrowSystemError(GetLastError(), ERROR);
+    ThrowWinError(GetLastError(), ERROR);
 }
 
 fmt::internal::UTF16ToUTF8::UTF16ToUTF8(fmt::WStringRef s) {
