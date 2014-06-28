a0e2f5224700559775223edecd4b58fae398cb4d
Victor Zverovich <victor.zverovich@gmail.com>
2014-06-28 12:05:32 -0700

    MSVC workaround.

--- test/macro-test.cc
+++ test/macro-test.cc
@@ -100,7 +100,8 @@ int TestVariadic(FMT_GEN(10, GET_TYPE), const fmt::ArgList &args) { \
     result += args[i].int_value; \
   return result;
 }
-FMT_VARIADIC(int, TestVariadic, FMT_GEN(10, GET_TYPE))
+FMT_VARIADIC(int, TestVariadic,
+    S<0>, S<1>, S<2>, S<3>, S<4>, S<5>, S<6>, S<7>, S<8>, S<9>)
 
 #define MAKE_ARG(n) S<n>()
 
