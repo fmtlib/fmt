533b9cf24f19d9efc4c8cc986f8fb9323e86864e
Victor Zverovich <victor.zverovich@gmail.com>
2014-07-26 21:14:40 -0700

    Add missing import.

--- doc/conf.py
+++ doc/conf.py
@@ -11,7 +11,7 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import sys, os, re
+import sys, os, re, subprocess
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
