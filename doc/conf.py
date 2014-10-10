88f3f538705e025e3c3be27934ab705d6988007a
Victor Zverovich <victor.zverovich@gmail.com>
2014-10-10 08:47:36 -0700

    Fix Sphinx config

--- doc/conf.py
+++ doc/conf.py
@@ -111,7 +111,6 @@ pygments_style = 'sphinx'
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 html_theme = 'bootstrap'
-html_theme_path = '.'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -131,7 +130,7 @@ html_theme_options = {
 }
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['.']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
