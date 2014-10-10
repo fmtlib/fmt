bb1926753d946294e44f8a8760a195be7ada267b
Victor Zverovich <victor.zverovich@gmail.com>
2014-10-10 06:10:52 -0700

    Try using the default theme

--- doc/conf.py
+++ doc/conf.py
@@ -110,7 +110,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'sphinxdoc'
+#html_theme = 'sphinxdoc'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
