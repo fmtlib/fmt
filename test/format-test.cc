8ea9f068c7a2090ad97e7a53a21d3094e39581b0
Victor Zverovich <victor.zverovich@gmail.com>
2014-11-07 07:58:49 -0800

    Convert tabs to spaces

--- test/format-test.cc
+++ test/format-test.cc
@@ -1433,6 +1433,6 @@ void print_error(const char *file, int line, const char *format,
 
 TEST(FormatTest, MaxArgs) {
   EXPECT_EQ("0123456789abcde",
-	    fmt::format("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}",
+            fmt::format("{}{}{}{}{}{}{}{}{}{}{}{}{}{}{}",
                         0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e'));
 }
