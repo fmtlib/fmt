c95908bd8c4b0a01a84d821674564145e8231f6d
Victor Zverovich <victor.zverovich@gmail.com>
2014-12-17 06:41:21 -0800

    Disable bogus MSVC warnings

--- posix.cc
+++ posix.cc
@@ -25,6 +25,9 @@
  SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
+// Disable bogus MSVC warnings.
+#define _CRT_SECURE_NO_WARNINGS
+
 #include "posix.h"
 
 #include <limits.h>
@@ -34,7 +37,6 @@
 #ifndef _WIN32
 # include <unistd.h>
 #else
-# define _CRT_SECURE_NO_WARNINGS
 # include <windows.h>
 # include <io.h>
 
