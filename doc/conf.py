a7d7e0daec4f17d3172a7d4e8359b342f3e5985e
Victor Zverovich <victor.zverovich@gmail.com>
2014-12-19 07:42:46 -0800

    Restore bootstrap theme

--- doc/conf.py
+++ doc/conf.py
@@ -116,15 +116,30 @@ primary_domain = 'cpp'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'cppformat_theme'
+html_theme = 'bootstrap'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {}
+html_theme_options = {
+  # HTML navbar class (Default: "navbar") to attach to <div>.
+  # For black navbar, do "navbar navbar-inverse"
+  #'navbar_class': "navbar navbar-inverse",
+
+  # Fix navigation bar to top of page?
+  # Values: "true" (default) or "false"
+  'navbar_fixed_top': "true",
+
+  # Location of link to source.
+  # Options are "nav" (default), "footer".
+  'source_link_position': "footer",
+  
+  # Render the next and previous page links in navbar. (Default: true)
+  'navbar_sidebarrel': False
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['.']
+html_theme_path = ['sphinx-bootstrap-theme']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
