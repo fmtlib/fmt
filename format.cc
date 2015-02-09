267382af395f152c8b83726151237291d8fa5aa6
Victor Zverovich <victor.zverovich@gmail.com>
2015-02-09 06:53:05 -0800

    Revert the experimental fix for print_colored that didn't work

--- format.cc
+++ format.cc
@@ -40,7 +40,6 @@
 #  include <cstring>
 # endif
 # include <windows.h>
-# include <io.h>  // for _get_osfhandle
 #endif
 
 using fmt::internal::Arg;
@@ -1113,7 +1112,9 @@ FMT_FUNC void fmt::print(std::ostream &os, StringRef format_str, ArgList args) {
 
 FMT_FUNC void fmt::print_colored(Color c, StringRef format, ArgList args) {
 #ifdef _WIN32
-  HANDLE handle = reinterpret_cast<HANDLE>(_get_osfhandle(_fileno(stdout)));
+  HANDLE handle = GetStdHandle(STD_OUTPUT_HANDLE);
+  if (handle == INVALID_HANDLE_VALUE)
+    FMT_THROW(WindowsError(GetLastError(), "cannot get output handle"));
   CONSOLE_SCREEN_BUFFER_INFO info_con;
   if (!GetConsoleScreenBufferInfo(handle, &info_con))
     FMT_THROW(WindowsError(GetLastError(), "cannot get console information"));
