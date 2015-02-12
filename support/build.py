609f33c94ae362568a36a97894c95b5fc9fead8b
Victor Zverovich <victor.zverovich@gmail.com>
2015-02-12 15:41:30 -0800

    Fix appveyor script

--- support/build.py
+++ support/build.py
@@ -6,8 +6,9 @@ import os
 
 env = os.environ
 build = env['BUILD']
+cmake_command = ['cmake', '-DFMT_EXTRA_TESTS=ON', '-DCMAKE_BUILD_TYPE=' + env['CONFIG']]
 if build == 'mingw':
   # Remove path to Git bin directory from $PATH because it breaks MinGW config.
   env['PATH'] = env['PATH'].replace(r'C:\Program Files (x86)\Git\bin', '')
-  generator = ['-GMinGW Makefiles']
-check_call(['cmake', '-DFMT_EXTRA_TESTS=ON', '-DCMAKE_BUILD_TYPE=' + env['CONFIG']] + generator, env=env)
+  cmake_command.append('-GMinGW Makefiles')
+check_call(cmake_command, env=env)
