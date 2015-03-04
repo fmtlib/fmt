a6864a7264926544d6f88148a0383adfdf078177
Victor Zverovich <victor.zverovich@gmail.com>
2015-03-04 07:07:13 -0800

    Fix CMake installation and enable biicode build

--- support/biicode-build.py
+++ support/biicode-build.py
@@ -6,10 +6,12 @@ from download import Downloader
 from subprocess import check_call
 
 os_name = os.environ['TRAVIS_OS_NAME']
+cmake_dir = 'cmake-dir'
 if os_name == 'linux':
   # Install newer version of CMake.
   bootstrap.install_cmake(
-    'cmake-3.1.1-Linux-i386.tar.gz', check_installed=False, download_dir=None)
+    'cmake-3.1.1-Linux-i386.tar.gz', check_installed=False,
+    download_dir=None, install_dir=cmake_dir, add_to_path=False)
   with Downloader().download('http://www.biicode.com/downloads/latest/ubuntu64') as f:
     check_call(['sudo', 'dpkg', '-i', f])
 elif os_name == 'osx':
@@ -20,7 +22,9 @@ project_dir = 'biicode_project'
 check_call(['bii', 'init', project_dir])
 cppformat_dir = os.path.join(project_dir, 'blocks/vitaut/cppformat')
 shutil.copytree('.', cppformat_dir,
-                ignore=shutil.ignore_patterns('biicode_project'))
+                ignore=shutil.ignore_patterns('biicode_project', cmake_dir))
 for f in glob.glob('support/biicode/*'):
   shutil.copy(f, cppformat_dir)
-check_call(['bii', 'cpp:build'], cwd=project_dir)
+env = os.environ.copy()
+env['PATH'] = '../{}:{}'.format(cmake_dir, env['PATH'])
+check_call(['bii', 'cpp:build'], cwd=project_dir, env=env)
