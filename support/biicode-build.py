14d1fb561ff1e9f280ba720fc3efb263cdd8ae83
Victor Zverovich <victor.zverovich@gmail.com>
2015-03-04 09:22:52 -0800

    Don't run biicode on Travis

--- support/biicode-build.py
+++ support/biicode-build.py
@@ -1,27 +1,8 @@
 #!/usr/bin/env python
 # Build the project with Biicode.
 
-import bootstrap, glob, os, shutil, sys
-from download import Downloader
+import glob, os, shutil
 from subprocess import check_call
-
-os_name = os.environ['TRAVIS_OS_NAME']
-if os_name != 'linux':
-  sys.exit()
-
-cmake_dir = 'cmake-dir'
-cmake_program = 'cmake'
-  
-# Install newer version of CMake.
-cmake_program = bootstrap.install_cmake(
-  'cmake-3.1.1-Linux-i386.tar.gz', check_installed=False, download_dir=None, install_dir='.')
-cmake_dir = glob.glob('cmake*')[0]
-with Downloader().download('http://www.biicode.com/downloads/latest/ubuntu64') as f:
-  check_call(['sudo', 'dpkg', '-i', f])
-
-env = os.environ.copy()
-env['PATH'] = os.path.abspath(os.path.dirname(cmake_program)) + ':' + env['PATH']
-env['CMAKE_ROOT'] = os.path.abspath(cmake_dir)
 
 project_dir = 'biicode_project'
 check_call(['bii', 'init', project_dir], env=env)
@@ -30,6 +11,4 @@ shutil.copytree('.', cppformat_dir,
                 ignore=shutil.ignore_patterns('biicode_project', cmake_dir))
 for f in glob.glob('support/biicode/*'):
   shutil.copy(f, cppformat_dir)
-print env['CMAKE_ROOT']
-check_call(['cmake', '--version'], env=env)
-#check_call(['bii', 'cpp:build'], cwd=project_dir, env=env)
+check_call(['bii', 'cpp:build'], cwd=project_dir, env=env)
