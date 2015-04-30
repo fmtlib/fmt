969d1cdbdcbb661007e46e0b095155b57858b847
vitaut <victor.zverovich@gmail.com>
2015-04-30 08:41:17 -0700

    Break long line

--- format.h
+++ format.h
@@ -485,7 +485,9 @@ inline int getsign(double value) {
   return sign;
 }
 inline int isinfinity(double x) { return !_finite(x); }
-inline int isinfinity(long double x) { return !_finite(static_cast<double>(x)); }
+inline int isinfinity(long double x) {
+  return !_finite(static_cast<double>(x));
+}
 #endif
 
 template <typename Char>
