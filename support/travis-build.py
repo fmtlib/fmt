44fabfc803e6fc4cd8d536ec858903294b64818e
vitaut <victor.zverovich@gmail.com>
2015-05-21 19:01:47 -0700

    Add diagnostics

--- support/travis-build.py
+++ support/travis-build.py
@@ -15,6 +15,7 @@ def rmtree_if_exists(dir):
 build = os.environ['BUILD']
 if build == 'Doc':
   travis = 'TRAVIS' in os.environ
+  print("Has key {}".format('KEY' in os.environ))
   # Install dependencies.
   if travis:
     check_call('curl -s https://deb.nodesource.com/gpgkey/nodesource.gpg.key | ' +
