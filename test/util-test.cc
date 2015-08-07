a8a2982b9aa9a2629e7e57e7daa5d982cc4264b9
vitaut <victor.zverovich@gmail.com>
2015-08-07 07:13:43 -0700

    Fix test

--- test/util-test.cc
+++ test/util-test.cc
@@ -752,7 +752,7 @@ TEST(UtilTest, UTF8ToUTF16Error) {
 TEST(UtilTest, UTF16ToUTF8Convert) {
   fmt::internal::UTF16ToUTF8 u;
   EXPECT_EQ(ERROR_INVALID_PARAMETER, u.convert(fmt::WStringRef(0, 0)));
-  EXPECT_EQ(ERROR_INVALID_PARAMETER, u.convert(fmt::WStringRef("foo", INT_MAX + 1u)));
+  EXPECT_EQ(ERROR_INVALID_PARAMETER, u.convert(fmt::WStringRef(L"foo", INT_MAX + 1u)));
 }
 #endif  // _WIN32
 
