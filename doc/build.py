ee1fdc575b136c279e53a6fb3da192f987b46509
vitaut <victor.zverovich@gmail.com>
2015-10-16 07:18:04 -0700

    Print alabaster version

--- doc/build.py
+++ doc/build.py
@@ -40,6 +40,7 @@ def build_docs():
   pip_install('michaeljones/breathe',
               '511b0887293e7c6b12310bb61b3659068f48f0f4')
   print(check_output(['pip', '--version']))
+  print(check_output(['pip', 'show', 'alabaster']))
   print(check_output(['sphinx-build', '--version']))
   print('PATH:', os.environ['PATH'])
   print(check_output(['which', 'sphinx-build']))
