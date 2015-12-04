7c60db1e24f7dc2ee983b0f1844896af2ed6f445
vitaut <victor.zverovich@gmail.com>
2015-12-04 14:12:42 -0800

    Fix a warning

--- format.h
+++ format.h
@@ -3383,7 +3383,9 @@ int parse_nonnegative_int(const Char *&s) {
     }
     value = new_value;
   } while ('0' <= *s && *s <= '9');
-  if (value > (std::numeric_limits<int>::max)())
+  // Convert to unsigned to prevent a warning.
+  unsigned max_int = (std::numeric_limits<int>::max)();
+  if (value > max_int)
     FMT_THROW(FormatError("number is too big"));
   return value;
 }
