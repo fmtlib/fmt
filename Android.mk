4d56c5ce4c88359bed67b5ef6fbf2876fd3366e2
Sergey <non_2001@mail.ru>
2016-02-04 12:46:59 +0300

    fix android build

--- Android.mk
+++ Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := cppformat_static
 LOCAL_MODULE_FILENAME := libcppformat
 
-LOCAL_SRC_FILES := format.cc
+LOCAL_SRC_FILES := cppformat/format.cc
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
