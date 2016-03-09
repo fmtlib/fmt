59607f5e9902b2ec51769255c974fe547416480a
vitaut <victor.zverovich@gmail.com>
2016-03-09 07:47:08 -0800

    Fix warnings on GCC 4.6.3

--- test/printf-test.cc
+++ test/printf-test.cc
@@ -337,12 +337,9 @@ void TestLength(const char *length_spec) {
   TestLength<T>(length_spec, -42);
   TestLength<T>(length_spec, min);
   TestLength<T>(length_spec, max);
-  using fmt::internal::check;
-  fmt::LongLong long_long_min = std::numeric_limits<fmt::LongLong>::min();
-  if (check(min >= 0) || check(static_cast<fmt::LongLong>(min) > long_long_min))
-    TestLength<T>(length_spec, fmt::LongLong(min) - 1);
+  TestLength<T>(length_spec, fmt::LongLong(min) - 1);
   fmt::ULongLong long_long_max = std::numeric_limits<fmt::LongLong>::max();
-  if (check(max < 0 || static_cast<fmt::ULongLong>(max) < long_long_max))
+  if (static_cast<fmt::ULongLong>(max) < long_long_max)
     TestLength<T>(length_spec, fmt::LongLong(max) + 1);
   TestLength<T>(length_spec, std::numeric_limits<short>::min());
   TestLength<T>(length_spec, std::numeric_limits<unsigned short>::max());
