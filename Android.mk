e569297d6a745a0aa3ea3cdca9d08755f0a49e17
Victor Zverovich <victor.zverovich@gmail.com>
2016-04-24 10:23:02 -0700

    Update Android config

--- Android.mk
+++ Android.mk
@@ -1,10 +1,10 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE := cppformat_static
-LOCAL_MODULE_FILENAME := libcppformat
+LOCAL_MODULE := fmt_static
+LOCAL_MODULE_FILENAME := libfmt
 
-LOCAL_SRC_FILES := cppformat/format.cc
+LOCAL_SRC_FILES := fmt/format.cc
 
 LOCAL_C_INCLUDES := $(LOCAL_PATH)
 LOCAL_EXPORT_C_INCLUDES := $(LOCAL_PATH)
