c5fe3eb87eb050ca919ccc0bae8444ec607b2166
Victor Zverovich <victor.zverovich@gmail.com>
2016-05-05 07:48:06 -0700

    Fix code bloat regression on gcc 5.3.1 with -std=c++11 (#315)

--- fmt/format.h
+++ fmt/format.h
@@ -2094,11 +2094,15 @@ struct ArgArray<N, true/*IsPacked*/> {
   
   template <typename Formatter, typename T>
   static Value make(const T &value) {
+#ifdef __clang__
     Value result = MakeValue<Formatter>(value);
     // Workaround a bug in Apple LLVM version 4.2 (clang-425.0.28) of clang:
     // https://github.com/fmtlib/fmt/issues/276
     (void)result.custom.format;
     return result;
+#else
+    return MakeValue<Formatter>(value);
+#endif
   }
 };
 
