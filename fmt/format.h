9c865560fd0b8b25752374871392d8d997083883
Victor Zverovich <victor.zverovich@gmail.com>
2016-05-07 07:10:40 -0700

    Fix a warning

--- fmt/format.h
+++ fmt/format.h
@@ -889,7 +889,8 @@ class ThousandsSep {
     if (++digit_index_ % 3 != 0)
       return;
     buffer -= sep_.size();
-    std::uninitialized_copy(sep_.data(), sep_.data() + sep_.size(), buffer);
+    std::uninitialized_copy(sep_.data(), sep_.data() + sep_.size(),
+                            internal::make_ptr(buffer, sep_.size()));
   }
 };
 
