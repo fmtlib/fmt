903357c85373dc3fe280f364c9cc18ddf03fd617
Victor Zverovich <victor.zverovich@gmail.com>
2016-07-14 06:47:07 -0700

    Break a long line

--- fmt/format.h
+++ fmt/format.h
@@ -662,8 +662,8 @@ void Buffer<T>::append(const U *begin, const U *end) {
 
 namespace internal {
 
-// A memory buffer for trivially copyable/constructible types with the first SIZE
-// elements stored in the object itself.
+// A memory buffer for trivially copyable/constructible types with the first
+// SIZE elements stored in the object itself.
 template <typename T, std::size_t SIZE, typename Allocator = std::allocator<T> >
 class MemoryBuffer : private Allocator, public Buffer<T> {
  private:
