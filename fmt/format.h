14d8534900c93ba9aa9fd26f09430f31462ea4b2
Manu343726 <Manu343726@gmail.com>
2017-08-20 13:58:05 +0200

    Explicitly cast range length to std::size_t to prevent conversion warnings

--- fmt/format.h
+++ fmt/format.h
@@ -742,7 +742,7 @@ template <typename T>
 template <typename U>
 void Buffer<T>::append(const U *begin, const U *end) {
   FMT_ASSERT(end >= begin, "negative value");
-  std::size_t new_size = size_ + (end - begin);
+  std::size_t new_size = size_ + static_cast<std::size_t>(end - begin);
   if (new_size > capacity_)
     grow(new_size);
   std::uninitialized_copy(begin, end,
