bbc3b99b3107a2a601350c2572c88dcea2cb9a4f
yelnats321 <elnar.dakeshov@gmail.com>
2017-09-03 13:26:21 -0400

    Move init out of class defn.

--- fmt/format.h
+++ fmt/format.h
@@ -1937,49 +1937,7 @@ class ArgMap {
   MapType map_;
 
  public:
-  FMT_API void init(const ArgList &args) {
-    if (!map_.empty())
-      return;
-    typedef internal::NamedArg<Char> NamedArg;
-    const NamedArg *named_arg = FMT_NULL;
-    bool use_values =
-        args.type(ArgList::MAX_PACKED_ARGS - 1) == internal::Arg::NONE;
-    if (use_values) {
-      for (unsigned i = 0;/*nothing*/; ++i) {
-        internal::Arg::Type arg_type = args.type(i);
-        switch (arg_type) {
-        case internal::Arg::NONE:
-          return;
-        case internal::Arg::NAMED_ARG:
-          named_arg = static_cast<const NamedArg*>(args.values_[i].pointer);
-          map_.push_back(Pair(named_arg->name, *named_arg));
-          break;
-        default:
-          /*nothing*/;
-        }
-      }
-      return;
-    }
-    for (unsigned i = 0; i != ArgList::MAX_PACKED_ARGS; ++i) {
-      internal::Arg::Type arg_type = args.type(i);
-      if (arg_type == internal::Arg::NAMED_ARG) {
-        named_arg = static_cast<const NamedArg*>(args.args_[i].pointer);
-        map_.push_back(Pair(named_arg->name, *named_arg));
-      }
-    }
-    for (unsigned i = ArgList::MAX_PACKED_ARGS;/*nothing*/; ++i) {
-      switch (args.args_[i].type) {
-      case internal::Arg::NONE:
-        return;
-      case internal::Arg::NAMED_ARG:
-        named_arg = static_cast<const NamedArg*>(args.args_[i].pointer);
-        map_.push_back(Pair(named_arg->name, *named_arg));
-        break;
-      default:
-        /*nothing*/;
-      }
-    }
-  }
+  void init(const ArgList &args);
 
   const internal::Arg *find(const fmt::BasicStringRef<Char> &name) const {
     // The list is unsorted, so just return the first matching name.
@@ -1991,6 +1949,51 @@ class ArgMap {
     return FMT_NULL;
   }
 };
+
+template <typename Char>
+void ArgMap<Char>::init(const ArgList &args) {
+  if (!map_.empty())
+    return;
+  typedef internal::NamedArg<Char> NamedArg;
+  const NamedArg *named_arg = FMT_NULL;
+  bool use_values =
+      args.type(ArgList::MAX_PACKED_ARGS - 1) == internal::Arg::NONE;
+  if (use_values) {
+    for (unsigned i = 0;/*nothing*/; ++i) {
+      internal::Arg::Type arg_type = args.type(i);
+      switch (arg_type) {
+      case internal::Arg::NONE:
+        return;
+      case internal::Arg::NAMED_ARG:
+        named_arg = static_cast<const NamedArg*>(args.values_[i].pointer);
+        map_.push_back(Pair(named_arg->name, *named_arg));
+        break;
+      default:
+        /*nothing*/;
+      }
+    }
+    return;
+  }
+  for (unsigned i = 0; i != ArgList::MAX_PACKED_ARGS; ++i) {
+    internal::Arg::Type arg_type = args.type(i);
+    if (arg_type == internal::Arg::NAMED_ARG) {
+      named_arg = static_cast<const NamedArg*>(args.args_[i].pointer);
+      map_.push_back(Pair(named_arg->name, *named_arg));
+    }
+  }
+  for (unsigned i = ArgList::MAX_PACKED_ARGS;/*nothing*/; ++i) {
+    switch (args.args_[i].type) {
+    case internal::Arg::NONE:
+      return;
+    case internal::Arg::NAMED_ARG:
+      named_arg = static_cast<const NamedArg*>(args.args_[i].pointer);
+      map_.push_back(Pair(named_arg->name, *named_arg));
+      break;
+    default:
+      /*nothing*/;
+    }
+  }
+}
 
 template <typename Impl, typename Char, typename Spec = fmt::FormatSpec>
 class ArgFormatterBase : public ArgVisitor<Impl, void> {
