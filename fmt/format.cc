708d9509ff05647ea1bfc3e2bb14706ef482eed3
yumetodo <yume-wikijp@live.jp>
2017-10-14 13:54:23 +0900

    fix(Clang CodeGen): remove warnings
    ./fmt/fmt/format.h(308,10): warning : unknown pragma ignored [-Wunknown-pragmas]
             ^
    1 warning generated.
    format.cc In file included from fmt\fmt\format.cc:28:
    fmt\fmt/format.h(308,10): warning : unknown pragma ignored [-Wunknown-pragmas]
             ^
    fmt\fmt\format.cc(165,17): warning : 'strerror' is deprecated: This
    function or variable may be unsafe. Consider using strerror_s instead.
    To disable deprecation, use _CRT_SECURE_NO_WARNINGS. See online help for
    details. [-Wdeprecated-declarations]
          buffer_ = strerror(error_code_);
                    ^
    C:\Program Files (x86)\Windows Kits\10\Include\10.0.10240.0\ucrt\string.h(178,24) :  note: 'strerror' has been explicitly marked deprecated here
    _ACRTIMP char* __cdecl strerror(
                           ^
    fmt\fmt\format.cc(78,37): warning : unused function 'strerror_s' [-Wunused-function]
    static inline fmt::internal::Null<> strerror_s(char *, std::size_t, ...) {
                                        ^
    3 warnings generated.
    
    refactor: use attribute to remove -Wunused-function warnings instead of dummy function call

--- fmt/format.cc
+++ fmt/format.cc
@@ -72,9 +72,11 @@
 
 // Dummy implementations of strerror_r and strerror_s called if corresponding
 // system functions are not available.
+FMT_MAYBE_UNUSED
 static inline fmt::internal::Null<> strerror_r(int, char *, ...) {
   return fmt::internal::Null<>();
 }
+FMT_MAYBE_UNUSED
 static inline fmt::internal::Null<> strerror_s(char *, std::size_t, ...) {
   return fmt::internal::Null<>();
 }
@@ -159,6 +161,11 @@ int safe_strerror(
             ERANGE : result;
     }
 
+#ifdef __c2__
+# pragma clang diagnostic push
+# pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#endif
+
     // Fallback to strerror if strerror_r and strerror_s are not available.
     int fallback(internal::Null<>) {
       errno = 0;
@@ -166,13 +173,15 @@ int safe_strerror(
       return errno;
     }
 
+#ifdef __c2__
+# pragma clang diagnostic pop
+#endif
+
    public:
     StrError(int err_code, char *&buf, std::size_t buf_size)
       : error_code_(err_code), buffer_(buf), buffer_size_(buf_size) {}
 
     int run() {
-      // Suppress a warning about unused strerror_r.
-      strerror_r(0, FMT_NULL, "");
       return handle(strerror_r(error_code_, buffer_, buffer_size_));
     }
   };
