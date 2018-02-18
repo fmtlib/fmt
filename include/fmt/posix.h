1e91f47b9230dee2f93f866d1501e24659f4c88d
hubslave <29800872+hubslave@users.noreply.github.com>
2018-02-19 00:43:35 +0200

    Don't define FMT_LOCALE on OpenBSD
    OpenBSD doesn't have strtod_l() (at least under the default
    headers), so the class Locale that the macro gates won't compile.

--- include/fmt/posix.h
+++ include/fmt/posix.h
@@ -353,7 +353,7 @@ class File {
 long getpagesize();
 
 #if (defined(LC_NUMERIC_MASK) || defined(_MSC_VER)) && \
-    !defined(__ANDROID__) && !defined(__CYGWIN__)
+    !defined(__ANDROID__) && !defined(__CYGWIN__) && !defined(__OpenBSD__)
 # define FMT_LOCALE
 #endif
 
