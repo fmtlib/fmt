e282d963fecb72f85d86b0b98858f2614aaa466a
Victor Zverovich <victor.zverovich@gmail.com>
2018-05-26 09:46:59 -0700

    Bump version

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -16,7 +16,7 @@
 #include <type_traits>
 
 // The fmt library version in the form major * 10000 + minor * 100 + patch.
-#define FMT_VERSION 50000
+#define FMT_VERSION 50100
 
 #ifdef __has_feature
 # define FMT_HAS_FEATURE(x) __has_feature(x)
