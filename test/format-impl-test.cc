c0854017028b57ce15e060cd2bd428b4c1caa177
Nicolas Lesser <blitzrakete@gmail.com>
2018-12-07 19:12:51 +0100

    Add some unit tests.

--- test/format-impl-test.cc
+++ test/format-impl-test.cc
@@ -228,4 +228,8 @@ TEST(ColorsTest, Colors) {
       stdout,
       fmt::print(fg(fmt::color::blue) | fmt::emphasis::bold, "blue/bold"),
       "\x1b[1m\x1b[38;2;000;000;255mblue/bold\x1b[0m");
+  EXPECT_WRITE(stderr, fmt::print(stderr, fmt::emphasis::bold, "bold error"),
+               "\x1b[1mbold error\x1b[0m");
+  EXPECT_WRITE(stderr, fmt::print(stderr, fg(fmt::color::blue), "blue log"),
+                 "\x1b[38;2;000;000;255mblue log\x1b[0m");
 }
