dde095fab82be6178b260bae605b837a193239a8
Victor Zverovich <victor.zverovich@gmail.com>
2019-01-13 08:13:38 -0800

    Revert "Reorder defines"
    This reverts commit 4a059914a965485cfac89145d7f5ba70612f60f0.

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -144,9 +144,6 @@
 #endif
 
 #ifndef FMT_BEGIN_NAMESPACE
-#  define FMT_BEGIN_NAMESPACE \
-    namespace fmt {           \
-    FMT_INLINE_NAMESPACE v5 {
 #  if FMT_HAS_FEATURE(cxx_inline_namespaces) || FMT_GCC_VERSION >= 404 || \
       FMT_MSC_VER >= 1900
 #    define FMT_INLINE_NAMESPACE inline namespace
@@ -160,6 +157,9 @@
       using namespace v5;     \
       }
 #  endif
+#  define FMT_BEGIN_NAMESPACE \
+    namespace fmt {           \
+    FMT_INLINE_NAMESPACE v5 {
 #endif
 
 #if !defined(FMT_HEADER_ONLY) && defined(_WIN32)
