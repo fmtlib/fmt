8f7780a4f6c26284ea29b4c0c5a00afc1e175dd6
Victor Zverovich <victor.zverovich@gmail.com>
2019-03-06 09:23:16 -0800

    Correct comment

--- include/fmt/ranges.h
+++ include/fmt/ranges.h
@@ -1,4 +1,4 @@
-// Formatting library for C++ - the core API
+// Formatting library for C++ - experimental range support
 //
 // Copyright (c) 2012 - present, Victor Zverovich
 // All rights reserved.
