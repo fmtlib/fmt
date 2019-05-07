26710110330a1d3d67cac87e9bea1146092fb41a
denchat <19730041+denchat@users.noreply.github.com>
2019-05-07 20:24:59 +0700

    Supress warning redefine _CRT_SECURE_NO_WARNINGS just in case

--- test/posix-mock-test.cc
+++ test/posix-mock-test.cc
@@ -7,7 +7,9 @@
 
 // Disable bogus MSVC warnings.
 #ifdef _MSC_VER
-#  define _CRT_SECURE_NO_WARNINGS
+#  ifndef _CRT_SECURE_NO_WARNINGS
+#    define _CRT_SECURE_NO_WARNINGS
+#  endif
 #endif
 
 #include "posix-mock.h"
