c3fa33314045cb4a132eef9a94524fb073a1e696
Johan Norberg <norberg_johan@hotmail.com>
2020-04-11 20:16:45 +0200

    Remove warning in core.h with when compiling with gcc and -Wshadow
    In file included from build/_deps/fmt-src/include/fmt/format.h:44:0,
                     from src/main.cpp:5:
    build/_deps/fmt-src/include/fmt/core.h: In member function ‘const T& fmt::v6::internal::dynamic_arg_list::push(const Arg&)’:
    build/_deps/fmt-src/include/fmt/core.h:1256:10: error: declaration of ‘node’ shadows a member of ‘fmt::v6::internal::dynamic_arg_list’ [-Werror=shadow]
         auto node = std::unique_ptr<typed_node<T>>(new typed_node<T>(arg));
              ^~~~
    build/_deps/fmt-src/include/fmt/core.h:1236:37: note: shadowed declaration is here
       template <typename = void> struct node {

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -1288,10 +1288,10 @@ class dynamic_arg_list {
 
  public:
   template <typename T, typename Arg> const T& push(const Arg& arg) {
-    auto node = std::unique_ptr<typed_node<T>>(new typed_node<T>(arg));
-    auto& value = node->value;
-    node->next = std::move(head_);
-    head_ = std::move(node);
+    auto new_node = std::unique_ptr<typed_node<T>>(new typed_node<T>(arg));
+    auto& value = new_node->value;
+    new_node->next = std::move(head_);
+    head_ = std::move(new_node);
     return value;
   }
 };
