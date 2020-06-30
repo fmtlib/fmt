ef8c9af400f937421b2c7a8230dc4fca2ec733b9
Tobias Hammer <github@der-hammer.info>
2020-06-30 10:18:51 +0200

    Add comment to explain why std::fprintf

--- include/fmt/format-inl.h
+++ include/fmt/format-inl.h
@@ -49,6 +49,8 @@ FMT_BEGIN_NAMESPACE
 namespace detail {
 
 FMT_FUNC void assert_fail(const char* file, int line, const char* message) {
+  // Use unchecked std::fprintf to avoid triggering another assertion when
+  // writing to stderr fails
   std::fprintf(stderr, "%s:%d: assertion failed: %s", file, line, message);
   // Chosen instead of std::abort to satisfy Clang in CUDA mode during device
   // code pass.
