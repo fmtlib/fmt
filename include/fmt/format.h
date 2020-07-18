b17d5c4f5d5c7dd9a0f5518f3a45e398098b2026
Victor Zverovich <viz@fb.com>
2020-07-18 08:31:38 -0700

    Fix a regression in handling digit separators (#1782)

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -1546,7 +1546,7 @@ template <typename OutputIt, typename Char, typename UInt> struct int_writer {
     int num_digits = count_digits(abs_value);
     int size = num_digits, n = num_digits;
     std::string::const_iterator group = groups.cbegin();
-    while (group != groups.cend() && num_digits > *group && *group > 0 &&
+    while (group != groups.cend() && n > *group && *group > 0 &&
            *group != max_value<char>()) {
       size += sep_size;
       n -= *group;
