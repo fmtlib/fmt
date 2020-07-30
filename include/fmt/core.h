21c8b5c1421ae04df00a97e4c396ffb7b26916bc
Victor Zverovich <viz@fb.com>
2020-07-30 07:16:15 -0700

    Report error on missing named argument (#1796)

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -1913,7 +1913,7 @@ template <typename Context> class basic_format_args {
   }
 
   template <typename Char> int get_id(basic_string_view<Char> name) const {
-    if (!has_named_args()) return {};
+    if (!has_named_args()) return -1;
     const auto& named_args =
         (is_packed() ? values_[-1] : args_[-1].value_).named_args;
     for (size_t i = 0; i < named_args.size; ++i) {
