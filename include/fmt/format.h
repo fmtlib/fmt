412a6581885da38a8fe267357f3dc3e412afc7e7
Martin Wührer <martin.wuehrer@artech.at>
2020-08-12 18:09:14 +0200

    include/fmt/format.h: int_writer: removed unnecessary iterator type re-declaration (prevents shadow-waringing in clang)

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -1630,7 +1630,6 @@ template <typename OutputIt, typename Char, typename UInt> struct int_writer {
                               make_checked(p, s.size()));
     }
     if (prefix_size != 0) p[-1] = static_cast<Char>('-');
-    using iterator = remove_reference_t<decltype(reserve(out, 0))>;
     auto data = buffer.data();
     out = write_padded<align::right>(out, specs, size, size, [=](iterator it) {
       return copy_str<Char>(data, data + size, it);
