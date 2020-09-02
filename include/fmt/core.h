59010a447016157c03d364ba03377efdd563696f
Mark Harris <mharris@nvidia.com>
2020-09-02 12:51:28 +1000

    Fix finding string_view on libstdc++ <= c++14

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -227,10 +227,11 @@
 
 // libc++ supports string_view in pre-c++17.
 #if (FMT_HAS_INCLUDE(<string_view>) &&                       \
-     (__cplusplus > 201402L || defined(_LIBCPP_VERSION))) || \
+     (__cplusplus > 201703L || (__cplusplus > 201402L && defined(_LIBCPP_VERSION)))) || \
     (defined(_MSVC_LANG) && _MSVC_LANG > 201402L && _MSC_VER >= 1910)
 #  include <string_view>
 #  define FMT_USE_STRING_VIEW
+// libstdc++ supports experimental/string_view in c++14
 #elif FMT_HAS_INCLUDE("experimental/string_view") && __cplusplus >= 201402L
 #  include <experimental/string_view>
 #  define FMT_USE_EXPERIMENTAL_STRING_VIEW
