61422eccafd2c414979cdf605a6903dcb4bd06b3
Axel Kohlmeyer <akohlmey@gmail.com>
2020-09-18 07:18:03 -0400

    Disable fallthrough attributes for the Intel compilers
    On MacOS and Linux the Intel compilers may be identified as the
    host compilers (Clang or GNU) but do not support the corresponding
    compiler specific fallthrough attributes.

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -70,9 +70,9 @@
 #endif
 
 #if __cplusplus == 201103L || __cplusplus == 201402L
-#  if defined(__clang__)
+#  if defined(__clang__) && !defined(__INTEL_COMPILER)
 #    define FMT_FALLTHROUGH [[clang::fallthrough]]
-#  elif FMT_GCC_VERSION >= 700 && !defined(__PGI) && \
+#  elif FMT_GCC_VERSION >= 700 && !defined(__PGI) && !defined(__INTEL_COMPILER) && \
       (!defined(__EDG_VERSION__) || __EDG_VERSION__ >= 520)
 #    define FMT_FALLTHROUGH [[gnu::fallthrough]]
 #  else
