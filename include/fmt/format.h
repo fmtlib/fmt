0e3c282f0638e4b201a70414deab10db3e842626
denchat <19730041+denchat@users.noreply.github.com>
2021-04-10 23:04:12 +0700

    Try remove FMT_INSTANTIATION_DEF_API
    Try remove FMT_INSTANTIATION_DEF_API from extern template

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -4027,7 +4027,7 @@ constexpr detail::udl_arg<wchar_t> operator"" _a(const wchar_t* s, size_t) {
 #endif  // FMT_USE_USER_DEFINED_LITERALS
 
 namespace detail {
-extern template struct FMT_INSTANTIATION_DEF_API basic_data<void>;
+extern template struct basic_data<void>;
 }  // namespace detail
 FMT_END_NAMESPACE
 
