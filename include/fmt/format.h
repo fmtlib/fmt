e2c1a2c01ef28f819321f470f78ad3ec82f8bc18
denchat <19730041+denchat@users.noreply.github.com>
2021-04-12 22:16:24 +0700

    Rename FMT_INSTANTIATION_DECL_API
    Rename FMT_INSTANTIATION_DEF_API to FMT_INSTANTIATION_DECL_API
    
    [MinGW] Fix dllexport of explicit template instantiation
    https://reviews.llvm.org/D61118

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -995,7 +995,7 @@ FMT_INLINE uint16_t bsr2log10(int bsr) {
   return data[bsr];
 }
 
-FMT_EXTERN template struct FMT_INSTANTIATION_DEF_API basic_data<void>;
+FMT_EXTERN template struct FMT_INSTANTIATION_DECL_API basic_data<void>;
 
 // This is a struct rather than an alias to avoid shadowing warnings in gcc.
 struct data : basic_data<> {};
