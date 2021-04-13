be4c43d29f98639e59e65decd0bcb7e9879331cd
denchat <19730041+denchat@users.noreply.github.com>
2021-04-13 12:31:37 +0700

    Fix issue of std::make_unsigned_t is not found
    and change the mask target into unsigned int
    
    Note:  This is odd because the `std::make_unsigned_t` is supposed to bethere in c++14.

--- test/format-test.cc
+++ test/format-test.cc
@@ -773,7 +773,7 @@ TEST(FormatterTest, SignNotTruncated) {
   wchar_t format_str[] = {L'{', L':',
                           '+' | static_cast<wchar_t>(
                             1 << fmt::detail::num_bits<char>() 
-                            & static_cast<typename std::make_unsigned_t<wchar_t> >(-1) ),
+                            & static_cast<typename std::make_unsigned<int>::type >(-1) ),
                           L'}', 0};
   EXPECT_THROW(format(format_str, 42), format_error);
 }
