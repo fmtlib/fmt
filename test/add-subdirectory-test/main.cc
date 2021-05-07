2665afb515c62b3ab978cf40fd7d1b154a5b3650
Victor Zverovich <victor.zverovich@gmail.com>
2021-05-07 16:42:02 -0700

    Cleanup add-subdirectory-test

--- test/add-subdirectory-test/main.cc
+++ test/add-subdirectory-test/main.cc
@@ -1,6 +1,5 @@
-#include "fmt/format.h"
+#include "fmt/core.h"
 
 int main(int argc, char** argv) {
-  for(int i = 0; i < argc; ++i)
-    fmt::print("{}: {}\n", i, argv[i]);
+  for (int i = 0; i < argc; ++i) fmt::print("{}: {}\n", i, argv[i]);
 }
