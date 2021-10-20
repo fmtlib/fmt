249f03bbb70a2116fbde6fd9a2fd6cd31db10c89
Axel Kohlmeyer <akohlmey@gmail.com>
2021-10-20 09:21:49 -0400

    do not detect LLVM based IBMXL compiler (on ppc) as clang (#2555)

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -18,7 +18,7 @@
 // The fmt library version in the form major * 10000 + minor * 100 + patch.
 #define FMT_VERSION 80001
 
-#ifdef __clang__
+#if defined (__clang__ ) && !defined(__ibmxl__)
 #  define FMT_CLANG_VERSION (__clang_major__ * 100 + __clang_minor__)
 #else
 #  define FMT_CLANG_VERSION 0
