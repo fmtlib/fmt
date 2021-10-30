6eaceb5f73f12943376c833a8433b38ff7faed10
Vladislav Shchapov <phprus@gmail.com>
2021-10-30 21:12:36 +0500

    Fix incompatible between docutils 1.18.0 and sphinx 3.3.0 (#2575)

--- doc/build.py
+++ doc/build.py
@@ -26,6 +26,8 @@ def create_build_env(venv_dir='virtualenv'):
   pip = Pip(venv_dir)
   pip.install('wheel')
   pip.install('six')
+  # See: https://github.com/sphinx-doc/sphinx/issues/9777
+  pip.install('docutils==0.17.1')
   pip.install('sphinx-doc/sphinx', 'v3.3.0')
   pip.install('michaeljones/breathe', 'v4.25.0')
 
