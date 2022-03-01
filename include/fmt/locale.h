33ecbc1056b9f17e8e6354faa929d0258687a95e
Saad Ahmed <65092551+SaadAhmedGit@users.noreply.github.com>
2022-03-01 23:40:02 +0500

    Update locale.h

--- include/fmt/locale.h
+++ include/fmt/locale.h
@@ -1,2 +1,2 @@
 #include "xchar.h"
-#warning fmt/locale.h is deprecated, include fmt/format.h or fmt/xchar.h instead
+//warning fmt/locale.h is deprecated, include fmt/format.h or fmt/xchar.h instead
