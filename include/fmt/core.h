4f9311e689b40618bed9e111df92da88e9c51eb3
Victor Zverovich <viz@fb.com>
2022-05-27 10:49:19 -0700

    Fix definition of error_handler::on_error

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -585,7 +585,7 @@ struct error_handler {
   constexpr error_handler(const error_handler&) = default;
 
   // This function is intentionally not constexpr to give a compile-time error.
-  FMT_NORETURN FMT_API void on_error(const char* message) {
+  FMT_NORETURN void on_error(const char* message) {
     throw_format_error(message);
   }
 };
