1f575fd5c90278bcf723f72737f0f63c1951bea3
Victor Zverovich <viz@fb.com>
2022-08-27 08:55:39 -0700

    Bump version

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -17,7 +17,7 @@
 #include <type_traits>
 
 // The fmt library version in the form major * 10000 + minor * 100 + patch.
-#define FMT_VERSION 90000
+#define FMT_VERSION 90100
 
 #if defined(__clang__) && !defined(__ibmxl__)
 #  define FMT_CLANG_VERSION (__clang_major__ * 100 + __clang_minor__)
