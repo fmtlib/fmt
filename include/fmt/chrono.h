cd7202e0399677942a5428a7db9dec1d3c8b5c45
Vladislav Shchapov <vladislav@shchapov.ru>
2022-10-17 02:04:55 +0500

    Fix overflow error (#3143)
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>
    
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- include/fmt/chrono.h
+++ include/fmt/chrono.h
@@ -1838,7 +1838,7 @@ struct chrono_formatter {
       } else {
         write(second(), 2);
         write_fractional_seconds<char_type>(
-            out, std::chrono::duration<Rep, Period>(val));
+            out, std::chrono::duration<rep, Period>(val));
       }
       return;
     }
