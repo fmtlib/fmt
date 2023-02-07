49e92d2fa15fe16bb06b1f3541a324b83ee8df73
Russell Greene <russell@shotover.com>
2023-02-07 16:12:58 -0600

    [msvc] fix warning about non-inline variable

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -680,7 +680,7 @@ FMT_CONSTEXPR inline auto utf8_decode(const char* s, uint32_t* c, int* e)
   return next;
 }
 
-constexpr uint32_t invalid_code_point = ~uint32_t();
+constexpr uint32_t FMT_INLINE_VARIABLE invalid_code_point = ~uint32_t();
 
 // Invokes f(cp, sv) for every code point cp in s with sv being the string view
 // corresponding to the code point. cp is invalid_code_point on error.
