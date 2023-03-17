e1720c0e5133bd0a895ad9198e98043d717e9a7c
tmartin-gh <38798827+tmartin-gh@users.noreply.github.com>
2023-03-17 11:35:35 -0700

    Fix CUDA nvcc warning fmt/include/fmt/core.h(295): warning #1675-D: unrecognized GCC pragma (#3352)

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -291,7 +291,7 @@
 
 // Enable minimal optimizations for more compact code in debug mode.
 FMT_GCC_PRAGMA("GCC push_options")
-#if !defined(__OPTIMIZE__) && !defined(__NVCOMPILER) && !defined(__LCC__)
+#if !defined(__OPTIMIZE__) && !defined(__NVCOMPILER) && !defined(__LCC__) && !defined(__CUDACC__)
 FMT_GCC_PRAGMA("GCC optimize(\"Og\")")
 #endif
 
