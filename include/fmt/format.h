a331dbfb65467dc7f1f529ade4860efa6d96b65b
Tobias Schlüter <TobiSchluter@users.noreply.github.com>
2023-06-27 03:35:29 +0900

    Fix type in assert message. (#3508)
    Reviewed all strings in the file and found no other typos.

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -3300,7 +3300,7 @@ FMT_CONSTEXPR20 auto format_float(Float value, int precision, float_specs specs,
       significand <<= 1;
     } else {
       // Normalize subnormal inputs.
-      FMT_ASSERT(significand != 0, "zeros should not appear hear");
+      FMT_ASSERT(significand != 0, "zeros should not appear here");
       int shift = countl_zero(significand);
       FMT_ASSERT(shift >= num_bits<uint64_t>() - num_significand_bits<double>(),
                  "");
