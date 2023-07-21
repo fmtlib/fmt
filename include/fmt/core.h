dbabb305c38a9220a4f0f4c78972e5b62c905167
Dmitry Alexeev <53860485+alexeedm@users.noreply.github.com>
2023-07-21 20:29:15 +0200

    Turn off error-producing NVCC workaround when using c++20 (#3544)

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -92,7 +92,7 @@
 #ifndef FMT_USE_CONSTEXPR
 #  if (FMT_HAS_FEATURE(cxx_relaxed_constexpr) || FMT_MSC_VERSION >= 1912 || \
        (FMT_GCC_VERSION >= 600 && FMT_CPLUSPLUS >= 201402L)) &&             \
-      !FMT_ICC_VERSION && !defined(__NVCC__)
+      !FMT_ICC_VERSION && (!defined(__NVCC__) || FMT_CPLUSPLUS >= 202002L)
 #    define FMT_USE_CONSTEXPR 1
 #  else
 #    define FMT_USE_CONSTEXPR 0
