40524a3595924bba11af685a87e071b2a524e64b
Giel van Schijndel <giel@mortis.eu>
2023-09-30 20:34:42 +0200

    fix: make nested_formatter constexpr default constructible

--- include/fmt/format.h
+++ include/fmt/format.h
@@ -4224,6 +4224,8 @@ struct nested_formatter {
   formatter<T> formatter_;
 
  public:
+  constexpr nested_formatter() : width_(0), align_(align_t::none) {}
+
   FMT_CONSTEXPR auto parse(format_parse_context& ctx) -> const char* {
     auto specs = detail::dynamic_format_specs<char>();
     auto it = parse_format_specs(
