d9063baf227882da0f48c761abcbb08247eb1296
Victor Zverovich <viz@meta.com>
2023-10-15 08:23:36 -0700

    Fix perf regression in ostream::print

--- include/fmt/os.h
+++ include/fmt/os.h
@@ -419,7 +419,7 @@ class FMT_API ostream {
     output to the file.
    */
   template <typename... T> void print(format_string<T...> fmt, T&&... args) {
-    vformat_to(detail::buffer_appender<char>(buffer_), fmt,
+    vformat_to(std::back_inserter(buffer_), fmt,
                fmt::make_format_args(args...));
   }
 };
