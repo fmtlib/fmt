69f01fe893e67a887e02267608a1693e41f57c50
Ivan Shapovalov <intelfx@intelfx.name>
2023-12-02 19:11:31 +0400

    test: use our std::chrono::days fallback instead of >=C++17 conditional

--- test/chrono-test.cc
+++ test/chrono-test.cc
@@ -462,9 +462,7 @@ TEST(chrono_test, format_default) {
             fmt::format("{}", std::chrono::duration<int, std::exa>(42)));
   EXPECT_EQ("42min", fmt::format("{}", std::chrono::minutes(42)));
   EXPECT_EQ("42h", fmt::format("{}", std::chrono::hours(42)));
-#  if defined(__cpp_lib_chrono) && __cpp_lib_chrono >= 201907L
-  EXPECT_EQ("42d", fmt::format("{}", std::chrono::days(42)));
-#  endif
+  EXPECT_EQ("42d", fmt::format("{}", days(42)));
   EXPECT_EQ(
       "42[15]s",
       fmt::format("{}", std::chrono::duration<int, std::ratio<15, 1>>(42)));
