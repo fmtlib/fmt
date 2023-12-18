9165434e5a3c0b2fc5dacfff87771a9638b5c94a
Vladislav Shchapov <vladislav@shchapov.ru>
2023-12-19 02:00:11 +0500

    Enable consteval in MSVC VS2019 version 16.10 (#3757)
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -227,8 +227,9 @@
         __apple_build_version__ >= 14000029L) &&                 \
        FMT_CPLUSPLUS >= 202002L) ||                              \
       (defined(__cpp_consteval) &&                               \
-       (!FMT_MSC_VERSION || _MSC_FULL_VER >= 193030704))
-// consteval is broken in MSVC before VS2022 and Apple clang before 14.
+       (!FMT_MSC_VERSION || FMT_MSC_VERSION >= 1929))
+// consteval is broken in MSVC before VS2019 version 16.10 and Apple clang
+// before 14.
 #    define FMT_CONSTEVAL consteval
 #    define FMT_HAS_CONSTEVAL
 #  else
