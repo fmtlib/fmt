3d84b45a22a9d21afacdb6b550c08c279184aeaa
Victor Zverovich <victor.zverovich@gmail.com>
2024-01-10 06:57:22 -0800

    Update core.h

--- include/fmt/core.h
+++ include/fmt/core.h
@@ -1,4 +1,4 @@
-// This file is only provided for compatibility an may be removed in future
+// This file is only provided for compatibility and may be removed in future
 // versions. Use fmt/base.h if you don't need fmt::format and fmt/format.h
 // otherwise.
 
