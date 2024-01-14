362b40c1a31eba747dbecc2e3b4e8f6570f73ead
Victor Zverovich <viz@meta.com>
2024-01-13 19:21:27 -0800

    Fix docs

--- doc/build.py
+++ doc/build.py
@@ -55,8 +55,8 @@ def build_docs(version='dev', **kwargs):
       GENERATE_RTF      = NO
       CASE_SENSE_NAMES  = NO
       INPUT             = {0}/args.h {0}/base.h {0}/chrono.h {0}/color.h \
-                          {0}/compile.h {0}/format.h {0}/os.h {0}/ostream.h \
-                          {0}/printf.h {0}/ranges.h {0}/xchar.h
+                          {0}/core.h {0}/compile.h {0}/format.h {0}/os.h \
+                          {0}/ostream.h {0}/printf.h {0}/ranges.h {0}/xchar.h
       QUIET             = YES
       JAVADOC_AUTOBRIEF = YES
       AUTOLINK_SUPPORT  = NO
