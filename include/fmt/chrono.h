88dd6de611253498750b1cf3c76f1885dc179858
Zaheen Jamil <zaheen.jamil@gmail.com>
2024-05-26 18:20:21 +1000

    Ran clang-format 17

--- include/fmt/chrono.h
+++ include/fmt/chrono.h
@@ -74,8 +74,8 @@ template <typename To, typename From,
           FMT_ENABLE_IF(!std::is_same<From, To>::value &&
                         std::numeric_limits<From>::is_signed ==
                             std::numeric_limits<To>::is_signed)>
-FMT_CONSTEXPR auto lossless_integral_conversion(const From from,
-                                                int& ec) -> To {
+FMT_CONSTEXPR auto lossless_integral_conversion(const From from, int& ec)
+    -> To {
   ec = 0;
   using F = std::numeric_limits<From>;
   using T = std::numeric_limits<To>;
@@ -104,8 +104,8 @@ template <typename To, typename From,
           FMT_ENABLE_IF(!std::is_same<From, To>::value &&
                         std::numeric_limits<From>::is_signed !=
                             std::numeric_limits<To>::is_signed)>
-FMT_CONSTEXPR auto lossless_integral_conversion(const From from,
-                                                int& ec) -> To {
+FMT_CONSTEXPR auto lossless_integral_conversion(const From from, int& ec)
+    -> To {
   ec = 0;
   using F = std::numeric_limits<From>;
   using T = std::numeric_limits<To>;
@@ -137,8 +137,8 @@ FMT_CONSTEXPR auto lossless_integral_conversion(const From from,
 
 template <typename To, typename From,
           FMT_ENABLE_IF(std::is_same<From, To>::value)>
-FMT_CONSTEXPR auto lossless_integral_conversion(const From from,
-                                                int& ec) -> To {
+FMT_CONSTEXPR auto lossless_integral_conversion(const From from, int& ec)
+    -> To {
   ec = 0;
   return from;
 }  // function
@@ -395,8 +395,8 @@ void write_codecvt(codecvt_result<CodeUnit>& out, string_view in_buf,
 }
 
 template <typename OutputIt>
-auto write_encoded_tm_str(OutputIt out, string_view in,
-                          const std::locale& loc) -> OutputIt {
+auto write_encoded_tm_str(OutputIt out, string_view in, const std::locale& loc)
+    -> OutputIt {
   if (detail::use_utf8() && loc != get_classic_locale()) {
     // char16_t and char32_t codecvts are broken in MSVC (linkage errors) and
     // gcc-4.
@@ -425,8 +425,8 @@ auto write_encoded_tm_str(OutputIt out, string_view in,
 
 template <typename Char, typename OutputIt,
           FMT_ENABLE_IF(!std::is_same<Char, char>::value)>
-auto write_tm_str(OutputIt out, string_view sv,
-                  const std::locale& loc) -> OutputIt {
+auto write_tm_str(OutputIt out, string_view sv, const std::locale& loc)
+    -> OutputIt {
   codecvt_result<Char> unit;
   write_codecvt(unit, sv, loc);
   return copy<Char>(unit.buf, unit.end, out);
@@ -434,8 +434,8 @@ auto write_tm_str(OutputIt out, string_view sv,
 
 template <typename Char, typename OutputIt,
           FMT_ENABLE_IF(std::is_same<Char, char>::value)>
-auto write_tm_str(OutputIt out, string_view sv,
-                  const std::locale& loc) -> OutputIt {
+auto write_tm_str(OutputIt out, string_view sv, const std::locale& loc)
+    -> OutputIt {
   return write_encoded_tm_str(out, sv, loc);
 }
 
@@ -503,8 +503,9 @@ auto fmt_duration_cast(std::chrono::duration<FromRep, FromPeriod> from) -> To {
 }
 
 template <typename Duration>
-auto to_time_t(std::chrono::time_point<std::chrono::system_clock, Duration>
-                   time_point) -> std::time_t {
+auto to_time_t(
+    std::chrono::time_point<std::chrono::system_clock, Duration> time_point)
+    -> std::time_t {
   // Cannot use std::chrono::system_clock::to_time_t since this would first
   // require a cast to std::chrono::system_clock::time_point, which could
   // overflow.
@@ -606,8 +607,9 @@ inline auto gmtime(std::time_t time) -> std::tm {
 }
 
 template <typename Duration>
-inline auto gmtime(std::chrono::time_point<std::chrono::system_clock, Duration>
-                       time_point) -> std::tm {
+inline auto gmtime(
+    std::chrono::time_point<std::chrono::system_clock, Duration> time_point)
+    -> std::tm {
   return gmtime(detail::to_time_t(time_point));
 }
 
@@ -2240,8 +2242,8 @@ struct formatter<year_month_day, Char> : private formatter<std::tm, Char> {
   }
 
   template <typename FormatContext>
-  auto format(year_month_day val,
-              FormatContext& ctx) const -> decltype(ctx.out()) {
+  auto format(year_month_day val, FormatContext& ctx) const
+      -> decltype(ctx.out()) {
     auto time = std::tm();
     time.tm_year = static_cast<int>(val.year()) - 1900;
     time.tm_mon = static_cast<int>(static_cast<unsigned>(val.month())) - 1;
@@ -2291,8 +2293,8 @@ struct formatter<std::chrono::duration<Rep, Period>, Char> {
   }
 
   template <typename FormatContext>
-  auto format(std::chrono::duration<Rep, Period> d,
-              FormatContext& ctx) const -> decltype(ctx.out()) {
+  auto format(std::chrono::duration<Rep, Period> d, FormatContext& ctx) const
+      -> decltype(ctx.out()) {
     auto specs = specs_;
     auto precision = specs.precision;
     specs.precision = -1;
@@ -2363,8 +2365,8 @@ struct formatter<std::chrono::local_time<Duration>, Char>
   }
 
   template <typename FormatContext>
-  auto format(std::chrono::local_time<Duration> val,
-              FormatContext& ctx) const -> decltype(ctx.out()) {
+  auto format(std::chrono::local_time<Duration> val, FormatContext& ctx) const
+      -> decltype(ctx.out()) {
     using period = typename Duration::period;
     if (period::num != 1 || period::den != 1 ||
         std::is_floating_point<typename Duration::rep>::value) {
@@ -2441,8 +2443,8 @@ template <typename Char> struct formatter<std::tm, Char> {
   }
 
   template <typename FormatContext>
-  auto format(const std::tm& tm,
-              FormatContext& ctx) const -> decltype(ctx.out()) {
+  auto format(const std::tm& tm, FormatContext& ctx) const
+      -> decltype(ctx.out()) {
     return do_format<FormatContext, std::chrono::seconds>(tm, ctx, nullptr);
   }
 };
