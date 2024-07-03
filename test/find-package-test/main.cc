9c6b3d25af75244e9c6daf9e029634db55da43c7
ClausKlein <claus.klein@arcormail.de>
2024-07-04 00:02:23 +0200

    Make it works with installed fmt package

--- test/find-package-test/main.cc
+++ test/find-package-test/main.cc
@@ -1,4 +1,8 @@
-#include "fmt/format.h"
+#ifdef FMT_MODULE
+import fmt;
+#else
+#include <fmt/format.h>
+#endif
 
 int main(int argc, char** argv) {
   for (int i = 0; i < argc; ++i) fmt::print("{}: {}\n", i, argv[i]);
