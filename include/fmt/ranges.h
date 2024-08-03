766fbd26454aaeb8bd5d9e996e468dd668af9a9a
Hugo Sales <hugo@visrez.com>
2024-08-03 20:48:08 +0000

    Add 'n' specifier for tuple and pair

--- include/fmt/ranges.h
+++ include/fmt/ranges.h
@@ -330,7 +330,14 @@ struct formatter<Tuple, Char,
   template <typename ParseContext>
   FMT_CONSTEXPR auto parse(ParseContext& ctx) -> decltype(ctx.begin()) {
     auto it = ctx.begin();
-    if (it != ctx.end() && *it != '}') report_error("invalid format specifier");
+    auto end = ctx.end();
+    if (it != end && detail::to_ascii(*it) == 'n') {
+      ++it;
+      set_brackets({}, {});
+      set_separator({});
+    }
+    if (it != end && *it != '}') report_error("invalid format specifier");
+    ctx.advance_to(it);
     detail::for_each(formatters_, detail::parse_empty_specs<ParseContext>{ctx});
     return it;
   }
