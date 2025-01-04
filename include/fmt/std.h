2c3a5698ef4956568d378148b6e271dd70868669
Vladislav Shchapov <vladislav@shchapov.ru>
2025-01-04 19:02:51 +0500

    Simplify a copying the fill from basic_specs
    Signed-off-by: Vladislav Shchapov <vladislav@shchapov.ru>

--- include/fmt/std.h
+++ include/fmt/std.h
@@ -696,7 +696,7 @@ template <typename T, typename Char> struct formatter<std::complex<T>, Char> {
 
     auto outer_specs = format_specs();
     outer_specs.width = specs.width;
-    detail::basic_specs_copy_fill<Char>(outer_specs, specs);
+    outer_specs.set_fill(specs);
     outer_specs.set_align(specs.align());
 
     specs.width = 0;
