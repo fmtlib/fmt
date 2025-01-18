9ff9c695db8aeadf70eb26498a0e1cdceeeb849a
Victor Zverovich <viz@meta.com>
2025-01-18 10:28:46 -0800

    Bump version

--- include/fmt/base.h
+++ include/fmt/base.h
@@ -21,7 +21,7 @@
 #endif
 
 // The fmt library version in the form major * 10000 + minor * 100 + patch.
-#define FMT_VERSION 110102
+#define FMT_VERSION 110103
 
 // Detect compiler versions.
 #if defined(__clang__) && !defined(__ibmxl__)
