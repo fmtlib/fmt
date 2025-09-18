f175eba531430320d85ee08322cb80c67cb87138
CrackedMatter <81803926+CrackedMatter@users.noreply.github.com>
2025-09-18 21:41:03 +0200

    Export is_compiled_string and operator""_cf

--- include/fmt/compile.h
+++ include/fmt/compile.h
@@ -15,9 +15,10 @@
 #include "format.h"
 
 FMT_BEGIN_NAMESPACE
+FMT_BEGIN_EXPORT
 
 // A compile-time string which is compiled into fast formatting code.
-FMT_EXPORT class compiled_string {};
+class compiled_string {};
 
 template <typename S>
 struct is_compiled_string : std::is_base_of<compiled_string, S> {};
@@ -58,6 +59,8 @@ template <detail::fixed_string Str> constexpr auto operator""_cf() {
 }
 }  // namespace literals
 #endif
+
+FMT_END_EXPORT
 
 namespace detail {
 
