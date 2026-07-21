827f66d49c611a0489df4af78a1efdbfd601a089
mccakit <117523238+mccakit@users.noreply.github.com>
2026-07-21 17:00:18 +0300

    lint fixes 2

--- include/fmt/fmt-c.h
+++ include/fmt/fmt-c.h
@@ -201,10 +201,10 @@ typedef enum {} fmt_signed_char;
     (fmt_arg[]) { FMT_MAP(FMT_MAKE_ARG, ##__VA_ARGS__) }
 #  define FMT_EXPAND(v) v
 
-#  define FMT_FORMAT_ARGS(fmt, ...)                               \
-    (fmt),                                                        \
-        FMT_EXPAND(FMT_VA_SELECT(FMT_MAKE_NULL, FMT_MAKE_ARGLIST, \
-                                 ##__VA_ARGS__)(__VA_ARGS__)),    \
+#  define FMT_FORMAT_ARGS(fmt, ...)                                        \
+    (fmt),                                                                 \
+        FMT_EXPAND(FMT_VA_SELECT(                                          \
+            FMT_MAKE_NULL, FMT_MAKE_ARGLIST, ##__VA_ARGS__)(__VA_ARGS__)), \
         FMT_NARG(, ##__VA_ARGS__)
 
 #  define fmt_format(buffer, size, fmt, ...) \
