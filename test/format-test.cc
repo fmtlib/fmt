b4b1657c2aa45cf08dcd7a76e2302bef60314148
Satyakam Mishra <mishra.satyakam03@gmail.com>
2026-09-04 16:17:58 +0000

    Fix debug format width calculation (#4902)

--- test/format-test.cc
+++ test/format-test.cc
@@ -1006,6 +1006,7 @@ TEST(format_test, width) {
 
 TEST(format_test, debug_presentation) {
   EXPECT_EQ(fmt::format("{:?}", ""), R"("")");
+  EXPECT_EQ(fmt::format("{:1?}", ""), R"("")");
 
   EXPECT_EQ(fmt::format("{:*<5.0?}", "\n"), R"(*****)");
   EXPECT_EQ(fmt::format("{:*<5.1?}", "\n"), R"("****)");
@@ -1748,6 +1749,7 @@ TEST(format_test, format_char) {
 
   EXPECT_EQ(fmt::format("{}", '\n'), "\n");
   EXPECT_EQ(fmt::format("{:?}", '\n'), "'\\n'");
+  EXPECT_EQ(fmt::format("{:6?}", 'a'), "'a'   ");
   EXPECT_EQ(fmt::format("{:x}", '\xff'), "ff");
 }
 
