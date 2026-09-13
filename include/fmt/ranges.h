af21761b8823900141998118f4d5728f14a13e4a
zhao0335 <3797723137@qq.com>
2026-09-14 00:15:28 +0800

    Add braces around range formatter condition

--- include/fmt/ranges.h
+++ include/fmt/ranges.h
@@ -515,9 +515,10 @@ struct formatter<
   using nonlocking = void;
 
   FMT_CONSTEXPR formatter() {
-    if FMT_CONSTEXPR20 (range_format_kind<R, Char>::value == range_format::set)
+    if FMT_CONSTEXPR20 (range_format_kind<R, Char>::value == range_format::set) {
       range_formatter_.set_brackets(detail::string_literal<Char, '{'>{},
                                     detail::string_literal<Char, '}'>{});
+    }
   }
 
   FMT_CONSTEXPR auto parse(parse_context<Char>& ctx) -> const Char* {
